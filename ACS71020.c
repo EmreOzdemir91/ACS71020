d93ace78a3f81e2e9c9dc9beb03ca0858ff92a9c
aniketpalu <64416568+aniketpalu@users.noreply.github.com>
2020-09-11 18:38:30 +0530

    (.c) file Uploaded and made changes in (.h) file

---
+++ ACS71020.c
@@ -0,0 +1,221 @@
+#include<ACS71020.h>
+#include<SPI.h>
+
+/********************************************************************
+ * @fn          getvalue(value_type typ)
+ *
+ * @brief       Get the property or value of energy
+ *
+ * @param       Value_type keyword to fetch the data
+ *
+ * @return      float value of the demanded data
+ */
+float getvalue(value_type typ){
+  long temp;
+  float register_val;
+
+  switch (typ)
+  {
+    /*Case to find Effective Current
+      Range of Vrms ouput is 0 : ~1
+        */
+  case irms:
+        ConnectbySPI();
+        temp = SPI.transfer(Vrms_Irms_address);
+        temp = temp >> 16;
+        register_val = convertToDecimal(temp);
+
+        register_val = register_val * Imax;
+        DisconnectbySPI();
+        return(register_val);
+        break;
+    /*Case to find Effective Voltage
+      Range of Vrms ouput is 0 : ~1
+        */ 
+  case vrms:
+        ConnectbySPI();
+        temp = SPI.transfer(Vrms_Irms_address);
+        temp = temp << 16;
+        register_val = convertToDecimal(temp);
+
+        register_val = temp*Vmax;
+        DisconnectbySPI();
+        return(register_val);
+        break;
+    /* Case to find Active Power
+        Range of Vrms ouput is -2 : ~2
+        */ 
+  case pactive:
+        ConnectbySPI();
+        temp = SPI.transfer(pactive);
+        if(temp/100000000000000000 == 1){
+        /*To see if value is positive or negative value is divided by 10^16 as output is 17bit long
+         Range of Active power is -2 : ~2
+         */
+        register_val = convertToDecimal(temp%100000000000000000);
+        }
+        else
+        {
+          register_val = convertToDecimal(temp);
+        }
+        DisconnectbySPI();
+        return(register_val);
+        break;
+    /* Case to find Apparent Power
+        Range of output Apparent power is 0 : ~2
+     */
+  case papparant:
+        ConnectbySPI();
+        temp = SPI.transfer(papparant);
+        register_val = convertToDecimal(temp);
+        
+        register_val = temp*Imax*Vmax;
+        DisconnectbySPI();
+        return(register_val);
+        break;
+    /*case to find reactive power
+      Range of reactive power is 0 : ~2
+      */
+  case preactive:
+        ConnectbySPI();
+        temp = SPI.transfer(preactive);
+        register_val = convertToDecimal(temp);
+        register_val = temp*Imax*Vmax;
+        DisconnectbySPI();
+        return(register_val);
+        break;
+    /*Case to find Power Factor
+      Range of Power Factor is  -2 : ~2
+      */
+  case pfactor:
+        ConnectbySPI();
+        temp = SPI.transfer(pfactor);
+        if(temp/10000000000 == 1){
+        //To see if value is positive or negative value is divided by 10^16 as output is 17bit long
+        register_val = convertToDecimal(temp%10000000000);
+        }
+        else{
+        register_val = convertToDecimal(temp);
+        }
+        DisconnectbySPI();
+        return(register_val);
+        break;
+    /*Number of samples of current and voltage used for calculations
+      Range 0 to 511
+      */
+  case numpstout:
+        ConnectbySPI();
+        temp = SPI.transfer(numpstout);
+        register_val = convertToDecimal(temp);
+        DisconnectbySPI();
+        return(register_val);
+        break;
+    /* To find Instantaneous voltage measurement
+     Rangs of vcodes is -1 : ~1
+    */
+  case vcodes:
+        ConnectbySPI();
+        temp = SPI.transfer(vcodes);
+        register_val = convertToDecimal(temp);
+        register_val = register_val*Vmax;
+        DisconnectbySPI();
+        return(register_val);
+        break;
+    /* To find Instantaneous current measurement
+       Rangs of vcodes is -2 : ~2
+       */
+  case icodes:
+        ConnectbySPI();
+        temp = SPI.transfer(icodes);
+        register_val = convertToDecimal(temp);
+        register_val = register_val*Imax;
+        DisconnectbySPI();
+        return(register_val);
+        break;
+  
+  default:
+      break;
+  }
+     
+}
+/********************************************************************
+ * @fn          ACS71020_ConnectbySPI()
+ *
+ * @brief       Connects to ACS71020 using SPI protocol
+ *
+ * @param       None
+ *
+ * @return      None
+ */
+void ACS71020_ConnectbySPI()
+{
+  pinMode(ChS, OUTPUT);
+  SPI.begin();
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setDataMode(SPI_MODE0);
+  digitalWrite(ChS, LOW);
+}
+/********************************************************************
+ * @fn          ACS71020_ConnectbySPI()
+ *
+ * @brief       Disconnects ACS71020 with MCU
+ *
+ * @param       None
+ *
+ * @return      None
+ */
+void ACS71020_DisconnectbySPI()
+{
+  digitalWrite(ChS, HIGH);
+}
+/********************************************************************
+ * @fn          convertToDecimal(long n)
+ *
+ * @brief       Converts output given by ACS71020 (Binary) to decimal
+ *
+ * @param       long n (Output from SPI.transfer())
+ *
+ * @return      float value in Decimal 
+ */
+float convertToDecimal(long n) {
+    float dec = 0;
+    int i = 0, rem;
+    while (n != 0) {
+        rem = n % 10;
+        n /= 10;
+        dec += rem * pow(2, i);
+        ++i;
+    }
+    return dec;
+}
+
+/********************************************************************
+ * @fn          convertToDecimal(long n)
+ *
+ * @brief       Converts output given by ACS71020 (Binary) to decimal
+ *
+ * @param       ACS71020_type , Chip Select , MOSI Pin of Master, MISO pin of Master, Full Scale Voltage
+ *
+ * @return      None 
+ */
+void ACS71020_setup(ACS71020_type typ, int CS , int mosi,int miso,float vmax)
+{   
+    ChS = CS;
+    MOSI = mosi;
+    MISO = miso;
+    Vmax = vmax;
+  switch(typ){                                // Imax is defined by model name
+    case ACS71020_15A:
+      Imax = 15.00;
+      break;
+    case ACS71020_30A:
+      Imax = 30.00;
+      break;
+    default:
+      Imax = 30.00    
+    }
+    pinMode(ChS, OUTPUT);
+    pinMode(MOSI, OUTPUT);
+    pinMode(MISO, INPUT);
+    
+}
--- ACS71020.h
+++ ACS71020.h
@@ -2,35 +2,54 @@
 #define ACS71020_h
 #include<Arduino.h>
 
+/*
+Addresses of registers
+*/ 
 
-enum ACS71020_type { ACS71020_15A,
-                      ACS71020_30A
-                  }; // Types of ACS71020
-enum value_type {irms,
- vrms,
- pactive,
- papparant,
- preactive,
- pfactor,
- numpstout,
- vcodes,
- icodes
-       }; // Types of ACS71020 Values
+#define Vrms_Irms_address     0x20                // Vrms, Irms 
+#define pactive               0x21                // Active Power
+#define papparant             0x22                // Apparant Power
+#define preactive             0x23                // Reactive Power
+#define pfactor               0x24                // Power Factor
+#define numpstout             0x25                // Number of Samples of Current and Voltage used for calculation
+#define vcodes                0x2A                // Instantaneous Voltage Measurement
+#define icodes                0x2B                // Instantaneous Current Measurement
 
-class ACS71020 {
-  public:
-    int ChS, MOSI, MISO,SCLK;
-    float Vmax, Imax;
-    ACS71020(ACS71020_type type, int CS , int mosi, int miso, int sclk, float vmax);
-    float getvalue(value_type value);
-    void setup();
-  private:
-    void ConnectbySPI();
-    void DisconnectbySPI();
-    float convertToDecimal(long n);
-};
+
+
+
+
+
+
+/*
+@ACS71020_type User defined datatypes to select the Model of ACS71020
+
+@value_type - User Defined datatype to select the property user wants to fetch
+*/
+enum ACS71020_type { ACS71020_15A, ACS71020_30A};                                               // Models of ACS71020
+enum value_type {irms,vrms,pactive,papparant,preactive,pfactor,numpstout,vcodes,icodes};        // Types of ACS71020 Values
+
+/*
+ChS = Chip Select,
+MOSI = Master Out Slave In Pin,
+MISO = Master In Slave Out Pin,
+SCLK = Serial Clock Pin
+Vmax = Full Scale Voltage
+Imax = Full Scale Current
+*/
+int ChS, MOSI, MISO,SCLK;            
+float Vmax, Imax;
+
+float getvalue(value_type value);
+void setup(ACS71020_type typ, int CS , int mosi,int miso,float vmax);
+void ConnectbySPI();
+void DisconnectbySPI();
+float convertToDecimal(long n);
+
 
 
 
 
 #endif
+
+
