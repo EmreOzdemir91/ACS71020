8d45dee3dfdd9550bd87d96e2fa67280897a713e
aniketpalu <64416568+aniketpalu@users.noreply.github.com>
2020-09-17 16:54:14 +0530

    Update ACS71020.c

--- ACS71020.c
+++ ACS71020.c
@@ -47,7 +47,7 @@ if(spiHandle == NULL)
 }
 else
 {
-       Display_printf(display, 0, 0, "Master SPI initialized\n");
+    printf(display, 0, 0, "Master SPI initialized\n");
 }
 // Setting Up transaction parameters
 spiTransaction.count = MSGSIZE;
